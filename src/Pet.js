d8c4845397715b15983cb6c9cc56712070753bad
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2021-04-10 01:04:38 +0530

    feat(Search Param)
    Signed-off-by: Aniruddha Shriwant <aniruddhashriwant@gmail.com>

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,3 @@
-/* const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-}; */
-
 const Pet = (props) => {
   return (
     <div>
