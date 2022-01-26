8087e96505518651dbdd6ae74628124d1ad77a59
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2022-01-26 20:55:17 +0530

    code refactored
    Signed-off-by: Aniruddha Shriwant <aniruddhashriwant@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import { useState, StrictMode } from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
-import ThemeContext from "./ThemeContext";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  const theme = useState("darkblue");
-  return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <Router>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    </ThemeContext.Provider>
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  ]);
 };
 
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
